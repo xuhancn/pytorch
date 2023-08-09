028e2aecbe34732dc59ea7350c27210a60ec95d2
Han, Xu <xu.han@intel.com>
2023-08-09 15:14:59 +0000

    update code.

--- torch/extension.h
+++ torch/extension.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#ifndef CPP_WRAPPER_MODULE
 // All pure C++ headers for the C++ frontend.
-// #include <torch/all.h>
+#include <torch/all.h>
+#endif
+
 // Python bindings for the C++ frontend (includes Python.h).
 #include <torch/python.h>
 
